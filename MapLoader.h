8c06b651a622dc64dbdea37014ad5889569f24cb
Hector Rivera Jr <kuri1098@mail.fresnostate.edu>
2019-03-17 16:06:45 -0700

    Update MapLoader.h

--- MapLoader.h
+++ MapLoader.h
@@ -1,1 +1,22 @@
+#ifndef MAPLOADER_H
+#define MAPLOADER_H
 
+#include<GL/gl.h>
+#include<TextureLoader.h>
+
+class MapLoader
+{
+	public:
+		MapLoader();
+		MapLoader(float,float,float,float, float); //constructor to initialize tex coord to cutom values
+		virtual ~MapLoader();
+
+		void drawBG(float width, float height);	//Function to draw square with background texture
+		void mapMovement(std::string direction); //Changes coordinates to move to different zones
+
+		float xMax, xMin, yMax, yMin; //Texture coordinates
+		float moveIncrement; //Float to determine how much the map will move by in the mapMove func
+		float xD, yD;    
+	protected:
+	private:
+}
