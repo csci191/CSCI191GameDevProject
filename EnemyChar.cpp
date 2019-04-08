c394271abee9aa788ecb8c08f3c40bbef7a482a0
John Her <39104734+Johndh27@users.noreply.github.com>
2019-04-07 17:10:02 -0700

    New updated Enemy Class

--- EnemyChar.cpp
+++ EnemyChar.cpp
@@ -3,50 +3,19 @@
 EnemyChar::EnemyChar()
 {
     //ctor
-
-     //  Bottom Left Corner
-    verticies[0].x = 0.0;
-    verticies[0].y = 0.0;
-    verticies[0].z = -1.0;
-
-    //  Bottom Right Corner
-    verticies[1].x = 1.0;
-    verticies[1].y = 0.0;
-    verticies[1].z = -1.0;
-
-    //  Top Right Corner
-    verticies[2].x = 1.0;
-    verticies[2].y = 1.0;
-    verticies[2].z = -1.0;
-
-    //  Top Left Corner
-    verticies[3].x = 0.0;
-    verticies[3].y = 1.0;
-    verticies[3].z = -1.0;
-
-    xMin = 0.0;
-    xMax = 0.0;
-
-    yMin = 0.0;
-    yMax = 0.0;
-
-    xPos = -0.5;
-    yPos = -0.5;
+    xPos = 0.0;
+    yPos = 0.0;
     zPos = -1.0;
 
-    xSize = 0.2;
-    ySize = 0.2;
+    xSize = 0.0;
+    ySize = 0.0;
 
-    eMax = 11;
-    eMin = 0;
+    rotateX = 0.0;
+    rotateY = 0.0;
+    rotateZ = 0.0;
 
-    actionTrigger = 0;
-
-    position.x = 0.3;
-    position.y = 0.3;
-    position.z = -1.0;
-
-    speed = 2;
+    xMove = 0.0;
+    yMove = 0.01;
 }
 
 EnemyChar::~EnemyChar()
@@ -54,219 +23,147 @@ EnemyChar::~EnemyChar()
     //dtor
 }
 
-void EnemyChar::drawEnemyChar()
+void EnemyChar::drawEnemy()
 {
-    TEnmChar->binder();
     glPushMatrix();
 
+    glTranslated(xPos, yPos, zPos);
+    glRotated(rotateX, 1.0, 0.0, 0.0);
+    glRotated(rotateY, 0.0, 1.0, 0.0);
+    glRotated(rotateZ, 0.0, 0.0, 1.0);
+
+    glBindTexture(GL_TEXTURE_2D, enemyTex);
     glScaled(xSize, ySize, 1.0);
-    //glScaled(1.0/(float)(gridSize), (1.0)/(float)(gridSize), 1.0);
 
     glBegin(GL_QUADS);
         glTexCoord2f(xMin, yMax);
-        glVertex3f(verticies[0].x, verticies[0].y, verticies[0].z);
+        glVertex3f(1.0, -1.0, 0.0);
 
         glTexCoord2f(xMax, yMax);
-        glVertex3f(verticies[1].x, verticies[1].y, verticies[1].z);
+        glVertex3f(-1.0, -1.0, 0.0);
 
         glTexCoord2f(xMax, yMin);
-        glVertex3f(verticies[2].x, verticies[2].y, verticies[2].z);
+        glVertex3f(-1.0, 1.0, 0.0);
 
         glTexCoord2f(xMin, yMin);
-        glVertex3f(verticies[3].x, verticies[3].y, verticies[3].z);
+        glVertex3f(1.0, 1.0, 0.0);
     glEnd();
 
     glPopMatrix();
 }
 
-void EnemyChar::enemycharInit(char*fileName)
+void EnemyChar::placeEnemy(float x, float y, float z)
 {
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-    TEnmChar->loadTexture(fileName);
-    TimeEnmChar->start();
-
-    //  Sprite Sheet Grid
-    xMin = 0.0;
-    xMax = 0.11111111111111;
-
-    yMin = 0.50;
-    yMax = 0.75;
+    xPos = x;
+    yPos = y;
+    zPos = z;
 }
 
-void EnemyChar::enemycharActions()
+void EnemyChar::enemyActions()
 {
-    switch(actionTrigger)
+     switch(actionEnemy)
     {
-    case 0:
-        {//stand still facing down
-            xMin = 0.0;
-            xMax = 0.1111111111111;
-
-            yMin = 0.5;
-            yMax = 0.75;
-        }
-        break;
-
-    case 1: //stand still facing up
-        {
-            xMin = 0.0;
-            xMax = 0.11111111111111;
-
-            yMin = 0.0;
-            yMax = 0.25;
-        }
-        break;
-
-    case 2: //stand still facing right
-        {
-            xMin = 0.0;
-            xMax = 0.11111111111111;
-
-            yMin = 0.75;
-            yMax = 1.0;
-        }
-        break;
-
-    case 3: //stand still facing left
-        {
-            xMin = 0.0;
-            xMax = 0.11111111111111;
-
-            yMin = 0.25;
-            yMax = 0.50;
-        }
-        break;
-
-    case 4: //go facing down
-        {
-            xMin = 0.0;
-            xMax = 0.1111111111111;
-
-            yMin = 0.5;
-            yMax = 0.75;
-            enemycharMove(1);
-            position.y -= 0.1 / 100 * speed;
-        }
-        break;
-
-    case 5: //go facing up
-        {
-            xMin = 0.0;
-            xMax = 0.11111111111111;
-
-            yMin = 0.0;
-            yMax = 0.25;
-            enemycharMove(1);
-            position.y += 0.1 / 100 * speed;
-        }
-        break;
-
-    case 6: //go facing right
-        {
-            xMin = 0.0;
-            xMax = 0.11111111111111;
-
-            yMin = 0.75;
-            yMax = 1.0;
-            enemycharMove(1);
-            position.x += 0.1 / 100 * speed;
-        }
-        break;
-
-    case 7: //go facing left
-        {
-            xMin = 0.0;
-            xMax = 0.11111111111111;
-
-            yMin = 0.25;
-            yMax = 0.50;
-            enemycharMove(1);
-            position.x -= 0.1 / 100 * speed;
-        }
-        break;
-
-    case 8: //go facing down 5x
-        {
-            xMin = 0.0;
-            xMax = 0.1111111111111;
-
-            yMin = 0.5;
-            yMax = 0.75;
-            enemycharMove(5);
-            position.y -= 0.2 / 100 * speed;
-        }
-        break;
-
-    case 9: //go facing up 5x
-        {
-            xMin = 0.0;
-            xMax = 0.11111111111111;
-
-            yMin = 0.0;
-            yMax = 0.25;
-            enemycharMove(5);
-            position.y += 0.2 / 100 * speed;
-        }
-        break;
-
-    case 10: //go facing right 5x
-        {
-            xMin = 0.0;
-            xMax = 0.11111111111111;
-
-            yMin = 0.75;
-            yMax = 1.0;
-            enemycharMove(5);
-            position.x += 0.2 / 100 * speed;
-        }
-        break;
-
-    case 11: //go facing left 5x
-        {
-            xMin = 0.0;
-            xMax = 0.11111111111111;
-
-            yMin = 0.25;
-            yMax = 0.50;
-            enemycharMove(5);
-            position.x -= 0.2 / 100 * speed;
-        }
-        break;
-
-    }
-}
-
-void EnemyChar::enemycharMove(int moves)
-{
-    if(TimeEnmChar->getTicks()>40)
-        {
-            for(int i = 0; i < moves; i++)
+        case 0: //go Right
+            if(TE -> getTicks()>80)
             {
-                xMin += (0.11111111111111);
-                xMax += (0.11111111111111);
-                if(xMax >= 1)
+                yMin = 0.75;
+                yMax = 1.0;
+
+                xMin += 0.11111111111111 / (float)Frames;
+                xMax += 0.11111111111111 / (float)Frames;
+
+                //xPos += 0.01;
+
+                if(xMax >= 1.0)
                 {
-                    xMax = 0.22222222222222;
-                    xMin = 0.11111111111111;
+                    xMin = 0.0;
+                    xMax = 0.11111111111111 / (float)Frames;
                 }
+
+                TE -> reset();
             }
-            TimeEnmChar->reset();
-        }
+        break;
+
+        case 1: //go Left
+            if(TE -> getTicks()>80)
+            {
+
+                yMin = 0.25;
+                yMax = 0.5;
+
+                xMin += 0.11111111111111 / (float)Frames;
+                xMax += 0.11111111111111 / (float)Frames;
+
+                //xPos += 0.01;
+
+                if(xMax >= 1.0)
+                {
+                    xMin = 0.0;
+                    xMax = 0.11111111111111 / (float)Frames;
+                }
+
+                TE -> reset();
+            }
+
+        break;
+
+        case 2: //go Down
+            if(TE -> getTicks()>80)
+            {
+
+                yMin = 0.5;
+                yMax = 0.75;
+
+                xMin += 0.11111111111111 / (float)Frames;
+                xMax += 0.11111111111111 / (float)Frames;
+
+                //xPos += 0.01;
+
+                if(xMax >= 1.0)
+                {
+                    xMin = 0.0;
+                    xMax = 0.11111111111111 / (float)Frames;
+                }
+
+                TE -> reset();
+            }
+
+        break;
+
+        case 3: //go Up
+            if(TE -> getTicks()>80)
+            {
+
+                yMin = 0.0;
+                yMax = 0.25;
+
+                xMin += 0.11111111111111 / (float)Frames;
+                xMax += 0.11111111111111 / (float)Frames;
+
+                //xPos += 0.01;
+
+                if(xMax >= 1.0)
+                {
+                    xMin = 0.0;
+                    xMax = 0.11111111111111 / (float)Frames;
+                }
+
+                TE -> reset();
+            }
+
+        break;
+    }
 }
 
-/*
-void EnemyChar::collision(Player* Ply)
+void EnemyChar::enemyInit()
 {
-    if(position.x = Ply->position.x)
-    {
+    Frames = 1;
 
-    }
+    xMin = 0.0;
+    xMax = 0.11111111111111 / (float)Frames;
 
-    if(position.y = Ply->position.y)
-    {
+    yMin = 0.0;
+    yMax = 0.25 / (float)Frames;
 
-    }
+    yPos = -0.8;
 }
-*/
