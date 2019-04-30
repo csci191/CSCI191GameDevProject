9246ca4f9e9074bc47eca45c1f17ba458f86114b
Hector Rivera Jr <kuri1098@mail.fresnostate.edu>
2019-04-29 18:17:32 -0700

    Update AdjacencyMatrix.cpp

--- AdjacencyMatrix.cpp
+++ AdjacencyMatrix.cpp
@@ -1,30 +1,76 @@
-#ifndef ADJACENCYMATRIX_H
-#define ADJACENCYMATRIX_H
-/*
-The Matrix is designed int the format [Rows][Columns]
-[0][0] is top left [0][9] is top right in a 10X10 Matrix
-*/
+#include "AdjacencyMatrix.h"
+#include <cmath>
+//irrKlang for sounds
 
-class AdjacencyMatrix
+//Default values are based on the project this class was designed for
+AdjacencyMatrix::AdjacencyMatrix()
 {
-    private:
-        int C; //columns
-        int R; //rows
-    public:
-        AdjacencyMatrix();
-        AdjacencyMatrix(int, int);
+	C = 100;
+	R = 100;
+	adjMtrx = new int *[R];
+	for (int i = 0; i < R; i++){
+		adjMtrx[i] = new int[C];
+	}
 
-        virtual ~AdjacencyMatrix();
+	for(int i = 0; i < R; i++){
+		for (int j = 0; j < C; j++){
+			adjMtrx[i][j] = 0;
+		}
+	}
+	createClearMap();
+}
 
-        void adjPush(int, int);
-        void adjPop(int, int);
-        void createClearMap();
+AdjacencyMatrix::AdjacencyMatrix(int columns, int rows)
+{
+	C = columns;
+	R = rows;
+	adjMtrx = new int *[R];
+	for (int i = 0; i < R; i++){
+		adjMtrx[i] = new int[C];
+	}
 
-        int **adjMtrx;
+	for(int i = 0; i < R; i++){
+		for (int j = 0; j < C; j++){
+			adjMtrx[i][j] = 0;
+		}
+	}
+}
 
-    protected:
+AdjacencyMatrix::~AdjacencyMatrix()
+{
+	for (int i = 0; i < R; i++){
+		delete [] adjMtrx[i];
+	}
+	delete [] adjMtrx;
+}
 
-    private:
-};
+AdjacencyMatrix::adjPush(int h, int v)
+{
+	if ( (h >= 0 && h < R) && (v >= 0 && h < C) ){
+		adjMtrx[h][v] = 1;
+	}
+}
 
-#endif 
+AdjacencyMatrix::adjPop(int h, int v)
+{
+	if ( (h >= 0 && h < R) && (v >= 0 && h < C) ){
+		adjMtrx[h][v] = 0;
+	}
+}
+
+AdjacencyMatrix::createClearMap()
+{
+	int temp = (int)sqrt(C);
+	adjPush(0,1);
+	adjPush(0,10);
+	adjPush(1,0);
+	adjPush(10,0);
+	for (int i = 1; i < C; i++){
+		if ( ((i + 1)%temp) != 0){
+			adjPush(i, i + 1);
+			adjPush(i+1, i);
+		}
+		    adjPush(i, i+10);
+		    adjPush(i+10, i);
+	}
+}
