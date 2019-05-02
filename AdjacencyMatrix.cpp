f7658ec0fd0ae04b996d18527e273a9ad9d3062a
Hector Rivera Jr <kuri1098@mail.fresnostate.edu>
2019-05-01 18:29:51 -0700

    Update AdjacencyMatrix.cpp

--- AdjacencyMatrix.cpp
+++ AdjacencyMatrix.cpp
@@ -5,55 +5,33 @@
 //Default values are based on the project this class was designed for
 AdjacencyMatrix::AdjacencyMatrix()
 {
-	C = 100;
-	R = 100;
-	adjMtrx = new int *[R];
-	for (int i = 0; i < R; i++){
-		adjMtrx[i] = new int[C];
-	}
-
-	for(int i = 0; i < R; i++){
-		for (int j = 0; j < C; j++){
-			adjMtrx[i][j] = 0;
-		}
-	}
+    int l = 0;
+    for (int i = 0; i < R; i++){
+        std::vector<int> v;
+        for(int j = 0; j < C; j++){
+            v.push_back(0);
+        }
+        adjMtrx.push_back(v);
+    }
 	createClearMap();
 }
 
-AdjacencyMatrix::AdjacencyMatrix(int columns, int rows)
-{
-	C = columns;
-	R = rows;
-	adjMtrx = new int *[R];
-	for (int i = 0; i < R; i++){
-		adjMtrx[i] = new int[C];
-	}
-
-	for(int i = 0; i < R; i++){
-		for (int j = 0; j < C; j++){
-			adjMtrx[i][j] = 0;
-		}
-	}
-}
 
 AdjacencyMatrix::~AdjacencyMatrix()
 {
-	for (int i = 0; i < R; i++){
-		delete [] adjMtrx[i];
-	}
-	delete [] adjMtrx;
+
 }
 
 void AdjacencyMatrix::adjPush(int h, int v)
 {
-	if ( (h >= 0 && h < R) && (v >= 0 && h < C) ){
+	if ( (h >= 0 && h < R) && (v >= 0 && v < C) ){
 		adjMtrx[h][v] = 1;
 	}
 }
 
 void AdjacencyMatrix::adjPop(int h, int v)
 {
-	if ( (h >= 0 && h < R) && (v >= 0 && h < C) ){
+	if ( (h >= 0 && h < R) && (v >= 0 && v < C) ){
 		adjMtrx[h][v] = 0;
 	}
 }
