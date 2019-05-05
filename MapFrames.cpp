717ac2f9a27619191ed9e528c7aaf3cdf9ddd78f
Hector Rivera Jr <kuri1098@mail.fresnostate.edu>
2019-05-04 18:00:27 -0700

    Update MapFrames.cpp

--- MapFrames.cpp
+++ MapFrames.cpp
@@ -1,12 +1,15 @@
 #include "MapFrames.h"
-#include<stdlib.h>
-#include<fstream>
-#include <stdio.h>
-#include <string.h>
+#include <math.h>
 
 MapFrames::MapFrames()
 {
-	frameCount = 0;
+    for (int i = 0; i < R; i++){
+        std::vector<int> v;
+        for(int j = 0; j < C; j++){
+            v.push_back(0);
+        }
+        adjMtrx.push_back(v);
+    }
 }
 
 MapFrames::~MapFrames()
@@ -15,11 +18,16 @@ MapFrames::~MapFrames()
 }
 
 /*Wil read map file and define inaccessable sections adjMtrx*/
-void MapFrames::initFrames(char *fileName)
+void MapFrames::initFrames(char *fileName, int searchFrame)
 {
+    createClearMap();
+
     float tempH = 0;
     float tempV = 0;
-    float currentF = 0;
+    frame = searchFrame;
+    bool currentFrame = false;
+    int tempFrame;
+
 	//File reading the C way
 	FILE * file = fopen(fileName, "r");
     if(file == NULL){
@@ -37,15 +45,74 @@ void MapFrames::initFrames(char *fileName)
         		   //else we move into the parser
 
         if ( strcmp( lineHeader, "i" ) == 0){
-        	fscanf(file, "%f\n", &frameCount);
-        	frames = new AdjacencyMatrix[(int)frameCount];
-        }else if (strcmp( lineHeader, "p" ) == 0){
+        	fscanf(file, "%f\n", &tempFrame);
+        	if (tempFrame == frame){
+                currentFrame = true;
+        	}
+        	else {
+                currentFrame = false;
+        	}
+        	//frames = (AdjacencyMatrix*)malloc(sizeof(int)*13);
+        }else if (strcmp( lineHeader, "p" ) == 0 && currentFrame == true){
         	fscanf(file, "%f %f\n", &tempH, &tempV);
-        	frames[(int)currentF].adjPop((int)tempH, (int)tempV);
-            frames[(int)currentF].adjPop((int)tempV, (int)tempH);
-        }else if(strcmp( lineHeader, "v" ) == 0){
-        	fscanf(file, "%f\n", currentF);
+        	adjPop((int)tempH, (int)tempV);
+            adjPop((int)tempV, (int)tempH);
         }
 
     }
 }
+
+void MapFrames::adjPush(int h, int v)
+{
+    if ( (h >= 0 && h < R) && (v >= 0 && v < C) ){
+		adjMtrx[h][v] = 1;
+	}
+}
+
+void MapFrames::adjPop(int h, int v)
+{
+    if ( (h >= 0 && h < R) && (v >= 0 && v < C) ){
+		adjMtrx[h][v] = 0;
+	}
+}
+
+void MapFrames::createClearMap()
+{
+int temp = (int)sqrt(C);
+	adjPush(0,1);
+	adjPush(0,10);
+	adjPush(1,0);
+	adjPush(10,0);
+	for (int i = 1; i < C; i++){
+		if ( ((i + 1)%temp) != 0){
+			adjPush(i, i + 1);
+			adjPush(i+1, i);
+		}
+		if ((i+10 <= 99))
+        {
+		    adjPush(i, i+10);
+		    adjPush(i+10, i);
+        }
+	}
+}
+
+bool MapFrames::checkConnection(int h, int v)
+{
+    if ( (adjMtrx[h][v] == 1) || (adjMtrx[v][h] == 1) ){
+        return true;
+    }else {
+    return false;}
+
+}
+
+void MapFrames::printMtrx()
+{
+    for (int i = 0; i < R; i++){
+        for (int j = 0; j < C; j++){
+            std::cout <<adjMtrx[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+}
+
