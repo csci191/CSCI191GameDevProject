7d79374deea42888e9602e347dd31aafe948469b
kaxiok <38771332+kaxiok@users.noreply.github.com>
2019-05-11 01:19:59 -0700

    Update GLScene.cpp

--- GLScene.cpp
+++ GLScene.cpp
@@ -8,11 +8,15 @@
 #include <EnemyChar.h>
 #include<ANIMATOR.H>
 
+#include<iostream>
+#include<string.h>
+using namespace std;
 
 MapLoader *Map1 = new MapLoader(0.0244, 0.0435,0.1463,0.2609,0.2174,0.1219); //yMin, xMin, yMax, xMax, xInc,yInc
 Player *Ply = new Player();
 Inputs *keyB = new Inputs();
 EnemyChar *EChar = new EnemyChar();
+
 /*     Enemy Texture     */
 textureLoader *ETex = new textureLoader();
 float tempx, tempy;
@@ -43,13 +47,18 @@ objects* arrow = new objects();            // ARROW OBJECT
 textureLoader* AT = new textureLoader();   // ARROW TEXTURE
 
 /*PLAYER HUD*/
-textureLoader HealthTex[2];
-objects PlayerHealth[3];
-textureLoader ManaTex[2];
-objects PlayerMana[6];
+textureLoader* HealthTex = new textureLoader();                     // health texture
+Animator PlayerHealth[3];
+textureLoader* ManaTex = new textureLoader();
+Animator PlayerMana[6];
+
+/* ATTACK */
 textureLoader* attackTex = new textureLoader();
 Animator* attackAnim = new Animator();
-objects* plyAnim = new objects();
+
+/* FIREBALL */
+textureLoader* fireballTex = new textureLoader();
+objects* fireball = new objects();
 
 GLScene::GLScene()
 {
@@ -72,7 +81,7 @@ GLint GLScene::initGL()
     glEnable(GL_COLOR_MATERIAL);
     //GLLight Light(GL_LIGHT0);
 
-/* INITIALIZE FOR START MENU */
+    /* INITIALIZE FOR START MENU */
     StartLogo->loadTexture("images/Game.png");         // STORE LOGO TEXTURE
     StartPress->loadTexture("images/PressSpace.png");
     StartBG->loadTexture("images/StartMenu.png");      // STORE PARALLAX TEXTURE
@@ -104,23 +113,37 @@ GLint GLScene::initGL()
     EChar->enemyInit();
 
     /*PLAYER HUD*/
-    HealthTex[0].loadTexture("images/FULLHEALTH.png");
-    HealthTex[1].loadTexture("images/NOHEALTH.png");
-    PlayerHealth[0].objectTex = HealthTex[1].tex;
-    PlayerHealth[1].objectTex = HealthTex[1].tex;
-    PlayerHealth[2].objectTex = HealthTex[1].tex;
+    /* HEALTH */
+    HealthTex[0].loadTexture("images/health.png");
+    for(int i=0; i<3; i++)
+    {
+        PlayerHealth[i].tex = HealthTex->tex;
+        PlayerHealth[i].xmin = 0.0;
+        PlayerHealth[i].xmax = 0.5;
+        PlayerHealth[i].show = true;
+        PlayerHealth[i].animInit((float)PlayerHealth[i].xmin, (float)PlayerHealth[i].xmax, 0, 1);
+    }
 
-    ManaTex[0].loadTexture("images/MANAFULL.png");
-    ManaTex[1].loadTexture("images/MANAEMPTY.png");
-    PlayerMana[0].objectTex = ManaTex[1].tex;
-    PlayerMana[1].objectTex = ManaTex[1].tex;
-    PlayerMana[2].objectTex = ManaTex[1].tex;
-    PlayerMana[3].objectTex = ManaTex[1].tex;
-    PlayerMana[4].objectTex = ManaTex[1].tex;
-    PlayerMana[5].objectTex = ManaTex[1].tex;
+    /* MANA */
+    ManaTex->loadTexture("images/mana.png");
+    for(int j=0; j<6; j++)
+    {
+        PlayerMana[j].tex = ManaTex->tex;
+        PlayerMana[j].xmin = 0.5;
+        PlayerMana[j].xmax = 1.0;
+        PlayerMana[j].show = true;
+        PlayerMana[j].animInit((float)PlayerMana[j].xmin, (float)PlayerMana[j].xmax, 0, 1);
+    }
 
+    /* ATTACK */
+    attackAnim->animInit((float)3/6, (float)4/6, (float)1/4, (float)2/4);
     attackTex->loadTexture("images/attack.png");
     attackAnim->tex = attackTex->tex;
+
+    /* FIREBALL */
+    fireballTex->loadTexture("images/fireball.png");
+    fireball->objectTex = fireballTex->tex;
+    fireball->show = false;
 
     Ply->currentPosition = Map1->objectPosition(Ply->position.x, Ply->position.y);
     Ply->prevPosition = Ply->currentPosition;
@@ -133,7 +156,7 @@ GLint GLScene::drawScene()
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
         glLoadIdentity();
 
-
+        /* BACKGROUND */
         glPushMatrix();
         StartPara->drawSquare(screenWidth,screenHeight);
         glPopMatrix();
@@ -165,6 +188,7 @@ GLint GLScene::drawScene()
             arrow->position.x = -6.4;
         }
 
+        /* ARROW CURSOR */
         glPushMatrix();
         glScaled(.05,.05,1);
         glTranslated(2.5,arrow->position.y,-1);
@@ -182,6 +206,28 @@ GLint GLScene::drawScene()
         glTranslated(-.5,-1.5,-1);
         MMObjects[1].drawObj();
         glPopMatrix();
+
+        /* RESETS HEALTH AND MANA */
+        Ply->health = 3;
+        Ply->mana = 6;
+
+        for(int i=0; i<3; i++)
+        {
+            PlayerHealth[i].tex = HealthTex->tex;
+            PlayerHealth[i].xmin = 0.0;
+            PlayerHealth[i].xmax = 0.5;
+            PlayerHealth[i].show = true;
+            PlayerHealth[i].animInit((float)PlayerHealth[i].xmin, (float)PlayerHealth[i].xmax, 0, 1);
+        }
+
+        for(int j=0; j<6; j++)
+        {
+            PlayerMana[j].tex = ManaTex->tex;
+            PlayerMana[j].xmin = 0.5;
+            PlayerMana[j].xmax = 1.0;
+            PlayerMana[j].show = true;
+            PlayerMana[j].animInit((float)PlayerMana[j].xmin, (float)PlayerMana[j].xmax, 0, 1);
+        }
     }
 
     else if(!mainMenu && !startMenu) // RENDERS MAIN GAME
@@ -189,47 +235,76 @@ GLint GLScene::drawScene()
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
         glLoadIdentity();
 
-    glPushMatrix();
-     glScaled(3.33,3.33,1.0);
-    Map1->drawBG(screenWidth, screenHeight);
-    glPopMatrix();
+        std::cout << Ply->mana << std::endl;
 
-    updateUI();
+        /* DRAWS BAGGROUND */
+        glPushMatrix();
+        glScaled(3.33,3.33,1.0);
+        Map1->drawBG(screenWidth, screenHeight);
+        glPopMatrix();
 
-    glPushMatrix();
-    glScaled(3.33,3.33,1.0);
-    Ply->drawPlayer();
-    glPopMatrix();
+        /* DRAWS HUD*/
+        updateUI(true, Ply->health);
+        updateUI(false, Ply->mana);
+        UI();
 
-    /* ATTACKS */
-    if(!pause){
-    glPushMatrix();
-    glScaled(.5,.5,1.0);
-    glTranslated(.1,.1,-2);
-    attackAnim->animDraw();
-    glPopMatrix(); }
+        /* DRAWS PLAYER */
+        glPushMatrix();
+        glScaled(3.33,3.33,1.0);
+        Ply->drawPlayer();
+        glPopMatrix();
 
-    glPushMatrix();
-    if(EChar->xPos < -0.1)
-    {
-        EChar->xMove = 0.001;
-        EChar->actionEnemy = 1;
-    }
-    else if (EChar->xPos > 0.1)
-    {
-        EChar->xMove = -0.001;
-        EChar->actionEnemy = 0;
-    }else if (EChar->yPos > 0.1)
-    {
+        /* DRAWS ATTACKS */
+        if(attackAnim->show)
+        {
+            glPushMatrix();
+            glScaled(1,1,1.0);
+            attackAnim->position.x = Ply->position.x -.3;                     // SETS POSITION TO WHERE PLAYERS X POSITION IS
+            attackAnim->position.y = Ply->position.y -.3;                     // SETS POSITION TO WHERE PLAYERS Y POSITION IS
+            glTranslated(attackAnim->position.x, attackAnim->position.y,-2);
+            attackAnim->animDraw();                                           // DRAWS THE ATTACK
+            attackAnim->play = true;
+            attackAnim->playAnimation();                                      // PLAYS THE ATTACK ANIMATION
+            glPopMatrix();
+        }
+
+        /* DRAWS FIREBALL */
+        if(fireball->show)
+        {
+            glPushMatrix();
+            glScaled(.1,.1,1);
+            if(!pause)
+                fireball->moveObj(fireball->objDir);
+
+            glTranslated(fireball->position.x, fireball->position.y,-2);
+            fireball->drawObj();
+            glPopMatrix();
+        }
+
+
+        glPushMatrix();
+        if(EChar->xPos < -0.1)
+        {
+            EChar->xMove = 0.001;
+            EChar->actionEnemy = 1;
+        }
+        else if (EChar->xPos > 0.1)
+        {
+            EChar->xMove = -0.001;
+            EChar->actionEnemy = 0;
+        }
+        else if (EChar->yPos > 0.1)
+        {
             EChar->yMove = -0.001;
             EChar->actionEnemy = 2;
-    }else if (EChar->yPos < -0.1)
-    {
-        EChar->yMove = 0.001;
-        EChar->actionEnemy = 3;
-    }
+        }
+        else if (EChar->yPos < -0.1)
+        {
+            EChar->yMove = 0.001;
+            EChar->actionEnemy = 3;
+        }
 
-    if(pause) // POPUP WINDOW FOR QUITTING
+        if(pause) // POPUP WINDOW FOR QUITTING
         {
             if(arrow->position.y < 1)
             {
@@ -251,58 +326,67 @@ GLint GLScene::drawScene()
             glEnd();
         }
 
-    glPushMatrix();
-    glScaled(3.33,3.33,1.0);
-    EChar->drawEnemy();
-    if(!pause){
-    EChar->enemyActions();
-    EChar->xPos += EChar->xMove;
-    EChar->yPos += EChar->yMove;
-    glPopMatrix();
-    }
-
-
+        glPushMatrix();
+        glScaled(3.33,3.33,1.0);
+        EChar->drawEnemy();
+        if(!pause)
+        {
+            EChar->enemyActions();
+            EChar->xPos += EChar->xMove;
+            EChar->yPos += EChar->yMove;
+            glPopMatrix();
+        }
     }
 }
 
-void GLScene::updateUI()
+void GLScene::UI()
 {
-    std::cout << Ply->health << std::endl;
     /* DISPLAY HEALTH */
     for(int a=0; a<3; a++)
     {
         glPushMatrix();
         glScaled(.05,.05,1.0);
         glTranslated(-15+a,7,-1);
-        PlayerHealth[a].drawObj();
-        //std::cout << PlayerHealth[a].objectTex << std::endl;
+        PlayerHealth[a].animDraw();
         glPopMatrix();
     }
 
-    /* UPDATE HEALTH */
-    for(int b=0; b<Ply->health;b++)
-    {
-        std::cout << PlayerHealth[b].objectTex << " " << HealthTex[0].tex<<std::endl;
-        PlayerHealth[b].objectTex = HealthTex[0].tex;
-    }
-
     /* DISPLAY MANA */
-    for(int i=0;i<6; i++)
+    for(int i=0; i<6; i++)
     {
         glPushMatrix();
         glScaled(.05,.05,1.0);
         glTranslated(-15+i,6,-1);
-        PlayerMana[i].drawObj();
+        PlayerMana[i].animDraw();
         glPopMatrix();
-    }
-
-    /* UPDATE MANA */
-    for(int j=0; j<Ply->mana; j++)
-    {
-        PlayerMana[j].objectTex = ManaTex[0].tex;
     }
 }
 
+void GLScene::updateUI(bool red, int point)
+{
+    /* UPDATES HEALTH UI */
+    if(red)
+    {
+        for(int i=0; i<3-point; i++)
+        {
+            PlayerHealth[i].xmin = 0.5;
+            PlayerHealth[i].xmax = 1.0;
+            PlayerHealth[i].animInit((float)PlayerHealth[i].xmin, (float)PlayerHealth[i].xmax, 0, 1);
+        }
+    }
+    /* UPDATES MANA UI */
+    else
+    {
+        for(int j=0; j<6-point; j++)
+        {
+            PlayerMana[j].tex = ManaTex->tex;
+            PlayerMana[j].xmin = 0.0;
+            PlayerMana[j].xmax = 0.5;
+            PlayerMana[j].animInit((float)PlayerMana[j].xmin, (float)PlayerMana[j].xmax, 0, 1);
+        }
+    }
+
+}
 
 GLvoid GLScene::resizeGLScene(GLsizei width, GLsizei height)
 {
@@ -317,73 +401,135 @@ GLvoid GLScene::resizeGLScene(GLsizei width, GLsizei height)
 
 int GLScene::winMsg(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-     switch (uMsg)									// Check For Windows Messages
-	{
+    switch (uMsg)									// Check For Windows Messages
+    {
 
-	    case WM_KEYDOWN:
-	        keyB->wParam = wParam;
-             if(!mainMenu && !startMenu)
+    case WM_KEYDOWN:
+        keyB->wParam = wParam;
+        if(!mainMenu && !startMenu)
+        {
+            if(!pause)
             {
-                //KbMs ->keyPressed(mdl);
-                if(!pause){keyB ->playerAction(Ply, Map1);}
+                keyB ->playerAction(Ply, Map1);
+
+                /* THIS HAS TO BE HERE TO WORK, IDK WHY */
+                switch(wParam)
+                {
+                case 'Z':
+                    /* UP ATTACK */
+                    if(Ply->ymin==0)
+                    {
+                        attackAnim->animInit((float)3/6, (float)4/6, (float)0/4, (float)1/4);
+                    }
+                    /* LEFT ATTACK */
+                    else if(Ply->ymin==.25)
+                    {
+                        attackAnim->animInit((float)3/6, (float)4/6, (float)1/4, (float)2/4);
+                    }
+                    /* DOWN ATTACK */
+                    else if(Ply->ymin==.5)
+                    {
+                        attackAnim->animInit((float)3/6, (float)4/6, (float)2/4, (float)3/4);
+                    }
+                    /* RIGHT ATTACK */
+                    else if(Ply->ymin==.75)
+                    {
+                        attackAnim->animInit((float)3/6, (float)4/6, (float)3/4, (float)4/4);
+                    }
+
+                    if(!attackAnim->show)
+                        attackAnim->show = true;
+                    break;
+                case 'X':
+                    if(Ply->mana>0)
+                    {
+                        Ply->mana -=1;
+                        updateUI(false, Ply->mana);
+
+                        fireball->position.x = 0;
+                        fireball->position.y = 0;
+                        if(Ply->ymin ==0)
+                        {
+                            fireball->objDir = "up";
+                        }
+                        else if(Ply->ymin == .25)
+                        {
+                            fireball->objDir = "left";
+                            fireball->xmax = -1;
+                        }
+                        else if(Ply->ymin == .5)
+                        {
+                            fireball->objDir = "down";
+                        }
+                        else if(Ply->ymin == .75)
+                        {
+                            fireball->objDir = "right";
+                            fireball->xmax = 1;
+                        }
+                        if(Ply->mana >0)
+                            fireball->show = true;
+                    }
+                    break;
+                }
             }
-            if(mainMenu || pause)
-            {
-                //arrow->position.y = keyB->keyArrow(arrow->position.y);
-		keyB->keyArroe2(arrow, mainMenu);
-            }
-            keyB->Menu(this,arrow->position.y);
-	    break;
-
-	    case WM_KEYUP:								// Has A Key Been Released?
-		{
-			keyB->wParam = wParam;
-			//Ply->actionTrigger = "stand";
-			//KbMs->keyUP();
-
-		break;								// Jump Back
-		}
-
-		case WM_LBUTTONDOWN:
-        {
-            //KbMs->wParam = wParam;
-            //KbMs->mouseEventDown(MdlTpt, LOWORD(lParam),HIWORD(lParam));
-        break;								// Jump Back
         }
-
-   		case WM_RBUTTONDOWN:
+        if(mainMenu || pause)
         {
-            //KbMs->wParam = wParam;
-            //KbMs->mouseEventDown(MdlTpt, LOWORD(lParam),HIWORD(lParam));
-        break;								// Jump Back
+            //arrow->position.y = keyB->keyArrow(arrow->position.y);
+            keyB->keyArroe2(arrow, mainMenu);
         }
+        keyB->Menu(this,arrow->position.y);
+        break;
 
-          case WM_MBUTTONDOWN:
-        {
-            //KbMs->wParam = wParam;
-            //KbMs->mouseEventDown(MdlTpt, LOWORD(lParam),HIWORD(lParam));
-        break;								// Jump Back
-        }
+    case WM_KEYUP:								// Has A Key Been Released?
+    {
+        keyB->wParam = wParam;
+        //Ply->actionTrigger = "stand";
+        //KbMs->keyUP();
 
-        case WM_LBUTTONUP:
-        case WM_RBUTTONUP:
-        case WM_MBUTTONUP:
-        {
-            //KbMs->mouseEventUp();
         break;								// Jump Back
-        }
+    }
 
-        case WM_MOUSEMOVE:
-        {
-             //KbMs->mouseEventMove(MdlTpt, LOWORD(lParam),HIWORD(lParam));
+    case WM_LBUTTONDOWN:
+    {
+        //KbMs->wParam = wParam;
+        //KbMs->mouseEventDown(MdlTpt, LOWORD(lParam),HIWORD(lParam));
         break;								// Jump Back
-        }
+    }
 
-        case WM_MOUSEWHEEL:
-        {
-            //KbMs->mouseWheel(MdlTpt, (double)GET_WHEEL_DELTA_WPARAM(wParam));
+    case WM_RBUTTONDOWN:
+    {
+        //KbMs->wParam = wParam;
+        //KbMs->mouseEventDown(MdlTpt, LOWORD(lParam),HIWORD(lParam));
         break;								// Jump Back
-        }
+    }
+
+    case WM_MBUTTONDOWN:
+    {
+        //KbMs->wParam = wParam;
+        //KbMs->mouseEventDown(MdlTpt, LOWORD(lParam),HIWORD(lParam));
+        break;								// Jump Back
+    }
+
+    case WM_LBUTTONUP:
+    case WM_RBUTTONUP:
+    case WM_MBUTTONUP:
+    {
+        //KbMs->mouseEventUp();
+        break;								// Jump Back
+    }
+
+    case WM_MOUSEMOVE:
+    {
+        //KbMs->mouseEventMove(MdlTpt, LOWORD(lParam),HIWORD(lParam));
+        break;								// Jump Back
+    }
+
+    case WM_MOUSEWHEEL:
+    {
+        //KbMs->mouseWheel(MdlTpt, (double)GET_WHEEL_DELTA_WPARAM(wParam));
+        break;								// Jump Back
+    }
 
     }
 }
