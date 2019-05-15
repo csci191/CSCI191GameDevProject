0d0284bfcda6bca93a0bbc471af119237e9b27e9
kaxiok <38771332+kaxiok@users.noreply.github.com>
2019-05-14 21:32:08 -0700

    Update objects.h

--- object updates/objects.h
+++ object updates/objects.h
@@ -1,6 +1,10 @@
 #ifndef OBJECTS_H
 #define OBJECTS_H
 #include<GL/gl.h>
+#include<iostream>
+#include<string.h>
+#include<PLAYER.H>
+using namespace std;
 
 typedef struct
 {
@@ -13,16 +17,21 @@ class objects
         objects();
         virtual ~objects();
         void drawObj();
-        void objInit(char*);
+        void objInit();
+        void moveObj(string, objects*, Player* p = NULL);
 
         GLuint objectTex;
 
         three verticies[4];
         three position;
+        string objDir;
         float xmin, ymin, xmax, ymax;
+        float startPos, endPos;
+        float maxDistanceX, maxDistanceY;
 
-
-
+        float xsize, ysize;
+        bool show;
+        bool moving;
     protected:
 
     private:
